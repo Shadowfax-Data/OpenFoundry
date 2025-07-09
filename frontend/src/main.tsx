13137b7671ba04167238301b9c316ddb841a647f
Di Wu <di.wu@shadowfaxdata.com>
2025-07-09 06:58:40 +0000

    hide app panel when screen too narrow

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,11 +5,13 @@ import { Provider } from "react-redux";
 import "./index.css";
 import { router } from "./router";
 import { store } from "./store";
+import { Toaster } from "@/components/ui/sonner";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
       <RouterProvider router={router} />
+      <Toaster />
     </Provider>
   </StrictMode>,
 );
